d6fb5598ce7881c0ca697119be08fc1ed217d07e
bslevin <blogs82@yahoo.com>
2022-06-12 18:17:40 +0000

    Create method that adds a expense item = innerhtml

--- js/run.js
+++ js/run.js
@@ -108,6 +108,23 @@ class BudgetApp {
             this.showBalance();
         }
     }
+    // Method to then add display balances on screen w/ expense objects as parameter
+    addExpenseItem(expenseObj) {
+        const div = document.createElement('div');
+        div.classList.add('expense');
+        div.innerHTML = `
+        <div class="expense-item d-flex justify-content-between align-items-baseline">
+           <h6 class="expense-title mb-0 text-uppercase list-item"><i class="fas fa-chevron-right"></i> ${expenseObj.expenseTitle}</h6>
+           <h5 class="total-expense-amount mb-0 list-item">€${expenseObj.expenseValue}</h5>
+           <div class="expense-icons list-item">
+            <a class="btn btn-primary edit-icon pt-2 btn-group" href="#" role="button" data-id="${expenseObj.expenseID}">Edit</a>
+            <a class="btn btn-primary delete-icon pt-2 btn-group" href="#" role="button" data-id="${expenseObj.expenseID}">Delete</a>
+           </div>
+          </div>
+        `
+        this.expenseList.appendChild(div);
+    }
+
 }
 
 /**
