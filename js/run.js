4c1fb46549d551683dad5f9e6bf7ea821e07f7bd
bslevin <blogs82@yahoo.com>
2022-06-12 16:17:36 +0000

    Added class properties to  select from the DOM

--- js/run.js
+++ js/run.js
@@ -5,6 +5,21 @@
 */
 class BudgetApp {
     constructor() {
-
+      this.budgetItemList = [];
+      this.budgetItemID = 0;
+      this.budgetFeedbackFail = document.querySelector(".budget-feedback-failed");
+      this.budgetFeedbackSuccess = document.querySelector(".budget-feedback-success");
+      this.expenseFeedbackFail = document.querySelector(".expense-feedback-failed");
+      this.expenseFeedbackSuccess = document.querySelector(".expense-feedback-success");
+      this.budgetForm = document.getElementById("budget-input-form");
+      this.budgetInput = document.getElementById("budget-input");
+      this.budgetAmount = document.getElementById("total-budget-amount");
+      this.expenseAmount = document.getElementById("total-expense-amount");
+      this.balance = document.getElementById("balance");
+      this.balanceAmount = document.getElementById("total-balance-amount");
+      this.expenseForm = document.getElementById("expense-form");
+      this.expenseInput = document.getElementById("expense-input");
+      this.amountInput = document.getElementById("amount-input");
+      this.expenseList = document.getElementById("expense-list");
     }
 }
